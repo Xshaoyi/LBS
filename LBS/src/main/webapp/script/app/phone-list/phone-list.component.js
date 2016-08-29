875eeef684e4b308bcf2c1d50d885781846f7fab
Xshaoyi <625799527@qq.com>
2016-08-29 19:08:38 +0800

    json

--- LBS/src/main/webapp/script/app/phone-list/phone-list.component.js
+++ LBS/src/main/webapp/script/app/phone-list/phone-list.component.js
@@ -1,19 +1,11 @@
-angular.
-  module('phoneList').
-  component('phoneList', {
-	  templateUrl: 'script/app/phone-list/phone-list.template.html',
-    controller: function PhoneListController() {
-      this.phones = [
-        {
-          name: 'Nexus S',
-          snippet: 'Fast just got faster with Nexus S.'
-        }, {
-          name: 'Motorola XOOM™ with Wi-Fi',
-          snippet: 'The Next, Next Generation tablet.'
-        }, {
-          name: 'MOTOROLA XOOM™',
-          snippet: 'The Next, Next Generation tablet.'
-        }
-      ];
-    }
-  });
+angular.module('phoneList').component('phoneList', {
+	templateUrl : 'script/app/phone-list/phone-list.template.html',
+	controller : [ '$http', function PhoneListController($http) {
+		var self = this;
+		self.orderProp = 'age';
+
+		$http.get('script/app/phones.json').then(function(response) {
+			self.phones = response.data;
+		});
+	} ]
+});
